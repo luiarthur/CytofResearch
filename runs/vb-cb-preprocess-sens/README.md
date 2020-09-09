47911b7c4a8d240e8e6c2cb70f389b4c6d1efae2
Arthur Lui <luiarthur@gmail.com>
2020-09-09 10:51:43 -0700

    added scripts to make data

---
+++ runs/vb-cb-preprocess-sens/README.md
@@ -0,0 +1,5 @@
+# README
+
+This directory contains scripts used for sensitivity analysis of the model to
+specification of the number of markers used, as determined by `p` for
+preprocessing.
---
+++ runs/vb-cb-preprocess-sens/preprocess_sens.jl
@@ -0,0 +1,69 @@
+import Pkg; Pkg.activate("../../")
+
+# Load libraries
+println("Loading libraries ...")
+import CSV
+using DataFrames
+include("../cb/PreProcess.jl")
+println("Finished loading libraries.")
+
+# Path to data.
+data_dir = "../data"
+path_to_data = "$(data_dir)/cb_transformed.csv"
+
+function separatesamples(Y)
+  sample_ids = sort(unique(Y.sample_id))
+  return Matrix.(select(Y[Y.sample_id .== i, :], Not(:sample_id))
+                 for i in sample_ids)
+end
+
+# Read transformed CB data into Y as a data frame.
+# Replace `missing` with `NaN`.
+function final_preprocess(Y; p=0.9, digits=3)
+  markernames = filter(x -> x != :sample_id, names(Y))
+  y = separatesamples(Y)
+  isgoodmarker = PreProcess.preprocess!(y, maxNanOrNegProp=p,
+                                        maxPosProp=p, rowThresh=-6.0)
+
+  includedmarkers = Symbol[]
+  excludedmarkers = Symbol[]
+
+  for j in 1:length(isgoodmarker)
+    marker = markernames[j]
+    if isgoodmarker[j]
+      append!(includedmarkers, [marker])
+    else
+      append!(excludedmarkers, [marker])
+    end
+  end
+
+  # Concatenate y and convert to data frame
+  Y_reduced = DataFrame(vcat(y...), includedmarkers)
+
+  # Convert NaN to missing
+  Y_reduced = ifelse.(isnan.(Y_reduced), missing, Y_reduced)
+
+  # Append a column for sample_id
+  Y_reduced[!, :sample_id] = vcat([fill(i, size(y[i], 1))
+                                   for i in 1:length(y)]...)
+
+  return round.(Y_reduced, digits=digits), excludedmarkers
+end
+
+# Read data
+Y = coalesce.(CSV.read(path_to_data), NaN)
+
+for p in (0.85, 0.9, 0.95, 0.99)
+  Y_reduced, excludedmarkers = final_preprocess(Y, p=p)
+  println("Excluded markers for p=$(p): $(excludedmarkers)")
+
+  # Path to reduced data
+  path_to_reduced_data = "$(data_dir)/cb_transformed_reduced_p$(p).csv"
+
+  # Write reduced data to disk.
+  println("Writing reduced-and-transformed CB data to: ")
+  println(path_to_reduced_data)
+
+  CSV.write(path_to_reduced_data, Y_reduced)
+end
+println("Done!")
