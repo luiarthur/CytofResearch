8e84d451b125190a126b78650b9d06f8e94c7071
Arthur Lui <luiarthur@gmail.com>
2020-09-11 15:19:40 -0700

    added subdir

--- runs/vb-cb-preprocess-sens/bundle.sh
+++ runs/vb-cb-preprocess-sens/bundle.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-fs=`tail -n10 results/best_seeds.txt | grep -oP "results/p\d+\.\d+_seed\d+"`
+RESULTS_DIR=$1
 
-mkdir -p results/bundle
-rm -rf results/bundle/*
+fs=`tail -n10 results/best_seeds.txt | grep -oP "$RESULTS_DIR/p\d+\.\d+_seed\d+"`
+
+mkdir -p $RESULTS_DIR/bundle
+rm -rf $RESULTS_DIR/bundle/*
 
 for f in $fs
 do
   dest=`echo $f | grep -oP "p\d+\.\d+"`
-  cp -r $f results/bundle/$dest
+  echo $dest
+  # cp -r $f $RESULTS_DIR/bundle/$dest
 done
